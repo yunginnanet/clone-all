5b30cd7884cf63c9dca3ead5f944abaf42e47780
kayos@tcp.direct <kayos@tcp.direct>
2024-06-08 02:50:19 -0700

    Fix: return to directory we started in

--- src/5-main.sh
+++ src/5-main.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+_pwd="$(pwd)"
 if ! setup "$@"; then
 	fatal "Failed to setup directory structure!"
 fi
 logln "Cloning all repos owned by $_USERNAME into ${_DESTINATION}/${_USERNAME}/"
 get
 echo -e "\e[1;32mfin.\e[0m"
+cd "$_pwd" || exit 1
